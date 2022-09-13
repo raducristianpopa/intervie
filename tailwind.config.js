9e84e51e965b2ea8e62ae6179511485f4ac7a111
Popa Radu <praducristian@gmail.com>
2022-09-14 00:08:40 +0300

    setup next app

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  // Add some basic Tailwind plugins to add additional features:
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+  ],
+};
