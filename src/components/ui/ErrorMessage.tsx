a90c7c899eb755eb2f2a648a7c0503d9270892ce
Popa Radu-Cristian <60383558+bearslair@users.noreply.github.com>
2022-09-20 03:03:22 +0300

    feat: add prettier-plugin-tailwindcss

--- src/components/ui/ErrorMessage.tsx
+++ src/components/ui/ErrorMessage.tsx
@@ -8,7 +8,7 @@ export function ErrorMessage({ title, error }: Props) {
 
 	return (
 		<div className="space-y-1 rounded-md border-2 border-red-500 border-opacity-50 bg-red-50 p-4 dark:bg-red-900 dark:bg-opacity-10">
-			{title && <h3 className="text-sm text-red-800 dark:text-red-200 font-medium">{title}</h3>}
+			{title && <h3 className="text-sm  font-medium text-red-800 dark:text-red-200">{title}</h3>}
 			<div className="text-sm text-red-700 dark:text-red-200 ">{error.message}</div>
 		</div>
 	);
